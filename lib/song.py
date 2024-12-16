ed65dcac564b48a1cf58edc6cb7bd0887ce4b846
mercy <mcherono529@gmail.com>
2024-12-16 10:24:15 +0300

    class-attribute

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,55 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+song1 = Song("99 Problems", "Jay-Z", "Rap")
+song2 = Song("Halo", "Beyonce", "Pop")
+song3 = Song("Empire State of Mind", "Jay-Z", "Rap")
+
+print(Song.count)  # Output: 3
+print(Song.genres)  # Output: ['Rap', 'Pop']
+print(Song.artists)  # Output: ['Jay-Z', 'Beyonce']
+print(Song.genre_count)  # Output: {'Rap': 2, 'Pop': 1}
+print(Song.artist_count)  # Output: {'Jay-Z': 2, 'Beyonce': 1}
